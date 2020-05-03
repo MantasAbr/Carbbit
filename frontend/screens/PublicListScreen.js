d6cc78fee9582885549ccd8bd9ef4a55787d782d
Mantas <mantasabra@gmail.com>
2020-05-03 18:20:21 +0300

    Designed the Upload Post screen

--- frontend/screens/PublicListScreen.js
+++ frontend/screens/PublicListScreen.js
@@ -29,7 +29,7 @@ export default class PublicList extends React.Component{
     }
 
     fetchJson(){
-        fetch('http://192.168.0.103:3000/posts/',{ // pasikeisti i savo IP
+        fetch('http://192.168.1.135:3000/posts/',{ // pasikeisti i savo IP
             method: 'GET',
             headers: {
               Accept: 'application/json',
