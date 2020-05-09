e2ebfcee10585968db09906fd5475093f36b7b52
Paulius Butkus <kaunaspb@gmail.com>
2020-05-09 21:47:57 +0300

    Remove unused imports

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-const fileUpload = require('express-fileupload');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,12 +14,6 @@ var rememberedRouter = require('./routes/remembered');
 var app = express();
 
 app.use(cors({credentials: true}));
-app.use(fileUpload({
-  createParentPath: true
-}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan('dev'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
