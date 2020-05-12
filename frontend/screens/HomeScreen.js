e8b9a01bb75a03e364267051edcf6fbd843ba5a1
5ulas <llaurynas24@gmail.com>
2020-05-12 22:43:23 +0300

    update, delete, registered user posts functions, session

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,13 +1,20 @@
-import * as React from 'react';
-import { TouchableOpacity, Platform, StyleSheet, View, Text, ImageBackground, Image } from 'react-native';
+import React, {useState} from 'react';
+import * as Google from 'expo-google-app-auth'
+import * as Facebook from 'expo-facebook';
+import { TouchableOpacity, Platform, StyleSheet, View, Text, ImageBackground, Image, Button, Alert, AsyncStorage  } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-
-import { MonoText } from '../components/StyledText';
-import { TitilliumWeb } from '../components/TitilliumWeb';
-
+import env from '../env/server'
 import Layout from '../constants/Layout'
 
 export default function HomeScreen({navigation}){
+
+  const [userLoginState, setUserLoginState] = React.useState( { signedIn: false , first_name: '', email: ''} );
+  const [users, setUsers] = useState([
+    { user_id: null, first_name: '', email: ''},
+  ]);
+  
+  let found = false;
+
     return (
         <View style={styles.container}>
             <Image
@@ -21,9 +28,153 @@ export default function HomeScreen({navigation}){
                 <TouchableOpacity onPress={() => handleButton_Register(navigation)} style={styles.helpLink}>
                     <Text style={styles.helpLinkText}>Registruotis</Text>
                 </TouchableOpacity>
+                <View style={{width: 150 , alignSelf: 'center', paddingTop: 20}}>
+                  <Button title="Sign in with Google" onPress={() => logInGoogle()} />
+                </View>
+                <View style={{width: 150 , alignSelf: 'center', paddingTop: 10}}>
+                  <Button title="Sign in with Facebook" onPress={() => logInFacebook()} />
+                </View>
             </ScrollView>
         </View>
     )
+    
+    async function fetchJsonCheckIfEmailExists() {
+      await fetch('http://' + env.server.ip + ':' + env.server.port + '/users/',{
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            }
+          })
+      .then((response) => response.json())
+      .then((responseJson) => {
+          for(var i = 0; i < responseJson.length; i++) {
+            setUsers(prevUsers => {
+              return [
+                { user_id: responseJson[i].user_id, first_name: responseJson[i].first_name, email: responseJson[i].email },
+                ...prevUsers
+              ];
+            });
+          }
+      for (let index = 0; index < users.length; index++) {
+        if (users[index].email == userLoginState.email) {
+          _storeData(users[index].user_id)
+          found = true;
+          return;
+        }
+      }
+      }).catch((error) =>{
+          console.error(error);
+      });
+      console.log(found)
+      if (!found) {
+        newUser(found);
+      }
+    }
+
+  function newUser(found) {
+    if (found == false) {
+      console.log(found);
+      var data = {
+        first_name: userLoginState.first_name,
+        email: userLoginState.email,
+        password: 'slaptazodis'
+      };
+      console.log('neradom');
+      fetch('http://' + env.server.ip + ':' + env.server.port + '/users/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      }).then(function (response) {
+        if (response.status >= 400) {
+          console.log('Couldn\'t post..');
+        }
+        return response.json();
+      }).then(function (data) {
+        console.log('Posted!')
+        console.log(data);
+        _storeData(data.id)
+      }).catch(function (err) {
+        console.log(err);
+      });
+    }
+  }
+
+    async function _storeData(id) {
+      try {
+        await AsyncStorage.setItem('user_id', id.toString() );
+        await AsyncStorage.setItem('username', userLoginState.first_name );
+        await AsyncStorage.setItem('email', userLoginState.email );
+      } catch (error) {
+        console.log(error)
+      }
+    };
+
+    async function _retrieveData() {
+      try {
+        const value = await AsyncStorage.getItem('user_id');
+        const value2 = await AsyncStorage.getItem('username');
+        const value3 = await AsyncStorage.getItem('email');
+        if (value !== null) {
+          console.log(value);
+          console.log(value2);
+          console.log(value3);
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    };
+
+    async function logInGoogle() {
+      try {
+        const result = await Google.logInAsync({
+          androidClientId:
+            "661974583477-h63i69p7v0t07f5b9auth50jjf54jg5l.apps.googleusercontent.com",
+          scopes: ["profile", "email"]
+        })
+    
+        if (result.type === "success") {
+          console.log('success')
+          console.log(result.user.name)
+          console.log(result.user.email)
+          await setUserLoginState({signedIn: true, first_name: result.user.name, email: result.user.email})
+          fetchJsonCheckIfEmailExists()
+          navigation.reset({
+            index: 0,
+            routes: [
+                {name: 'Root'}
+            ]
+        })
+        } else {
+          console.log("cancelled")
+        }
+      } catch (e) {
+        console.log("error", e)
+      }
+    }
+
+    async function logInFacebook() {
+      try {
+        await Facebook.initializeAsync('2847743641946575');
+        const {
+          type,
+          token
+        } = await Facebook.logInWithReadPermissionsAsync({
+          permissions: ['public_profile'],
+        });
+        if (type === 'success') {
+          const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+          let name = await response.json().name
+          let email = await response.json().email
+          await setUserLoginState({signedIn: true, first_name: name, email: email})
+          fetchJsonCheckIfEmailExists
+        } else {
+          console.log('Unable to login!')
+        }
+      } catch ({ message }) {
+        alert(`Facebook Login Error: ${message}`);
+      }
+    }
 }
 
 function handleButton_Login(navigation){
--- frontend/screens/UserPostsScreen.js
+++ frontend/screens/UserPostsScreen.js
@@ -1,25 +1,175 @@
-import * as React from 'react'
-import { TouchableOpacity, StyleSheet, View, Platform, ImageBackground, Modal, Image} from 'react-native';
-import { ScrollView} from 'react-native-gesture-handler';
-import { TitilliumWeb } from '../components/TitilliumWeb';
-import {Dimensions } from "react-native";
-import IonicsIcon from '../components/IonicsIcon';
+import * as React from 'react';
+import { render } from 'react-dom';
+import { View, Text, ActivityIndicator, StyleSheet, TouchableOpacity, TextInput, ImageBackground, Image, RefreshControl, AsyncStorage, Button } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import moment from "moment";
 
-export default class UserPostScreen extends React.Component{
+import env from '../env/server';
+export default class PublicList extends React.Component{
+    
+    state = {
+        isLoading: false,
+        results: [
+            {
+                post_id: '',
+                picture_uri: '',
+                body: '',
+                available_from_date: '',
+                available_to_date: '',
+                brand: '',
+                model: '',
+                user_id: ''
+            }
+        ]
+    };
 
-    render(){
+    componentDidMount(){
+        this.fetchJson();
+    }
+
+    fetchPosts = () => {
+        this.setState({isLoading: true});
+        fetch('http://' + env.server.ip + ':' + env.server.port + '/posts/',{
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            }
+          })
+      .then((response) => response.json())
+      .then((responseJson) => {
+         this.setState({ isLoading: false, results: responseJson})
+      })
+      .catch((error) =>{
+          console.error(error);
+          this.setState({isLoading: false});
+      });
+    }
+
+
+    handleDeletion = async (key) => {
+        console.log(key)
+        fetch('http://' + env.server.ip + ':' + env.server.port + '/posts/' + key, {
+        method: 'DELETE', 
+        })
+        .then(res => res.text())
+        .then(res => console.log(res))
+        this.fetchJson()
+    }
+
+
+    updatePost = async (key) => { 
+        const userID = await AsyncStorage.getItem('user_id');
+
+            var data = {
+                picture_uri: 'atnaujinom',
+                body: 'atnaujinom',
+                available_from_date: moment(this.state.availableFromDate).format('YYYY-MM-DD, HH:mm'),
+                available_to_date: moment(this.state.availableToDate).format('YYYY-MM-DD, HH:mm'),
+                brand: 'atnaujinom3',
+                model: 'atnaujinom',
+                is_private: false,
+                user_id: userID
+            }
+            fetch('http://' + env.server.ip + ':' + env.server.port + '/posts/' + key, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data)
+            }).then(function(response) {
+                if (response.status >= 400) {
+                console.log('Couldn\'t update..')
+                }
+                return response.json();
+            }).then(function(data) {
+                console.log(data)    
+                if(data == "success"){
+                console.log('updated!')
+                }
+            }).catch(function(err) {
+                console.log(err)
+            });
+            this.fetchJson()
+    }
+
+    fetchJson = async () => {
+        const userID = await AsyncStorage.getItem('user_id');
+        this.setState({isLoading: true});
+        fetch('http://' + env.server.ip + ':' + env.server.port + '/posts/user/' + userID,{
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            }
+          })
+      .then((response) => response.json())
+      .then((responseJson) => {
+         this.setState({ isLoading: false, results: responseJson})
+      })
+      .catch((error) =>{
+          console.error(error);
+          this.setState({isLoading: false});
+      });
+    }
+
+    render() {
+        if(this.state.isLoading){
+            return(
+                <ImageBackground source={require('../assets/backgrounds/kaunas_bg.png')} 
+                                style={styles.background} blurRadius={5}>
+                    <View style={{flex:1,padding:20}}>
+                        <ActivityIndicator/>
+                    </View>
+                </ImageBackground>
+            )
+        }
         return(
-            <ImageBackground source={require('../assets/backgrounds/klaipeda_bg.png')} 
-            style={styles.background} blurRadius={5}>
+            <ImageBackground source={require('../assets/backgrounds/kaunas_bg.png')} 
+                                style={styles.background} blurRadius={5}>
 
+                <View style={{flex:1,paddingTop:20}}>
+                    <Text style={styles.crud_header} >Posts:</Text>
+                    <FlatList
+                        refreshControl={ // pulldown refresh
+                            <RefreshControl
+                                isLoading={this.state.isLoading}
+                                onRefresh={this.fetchJson}
+                            />
+                        }
+                        data={this.state.results}
+                        renderItem={({item}) =>
+                            <View style={{borderColor:'black', borderWidth: 1, marginTop: 30} }>
+                                <Text style={{fontWeight:"bold"}}>{item.brand} {item.model}</Text>
+                                <Text>nuo {moment(item.available_to_date).format('YYYY-MM-DD, HH:mm')} </Text>
+                                <Text>iki {moment(item.available_from_date).format('YYYY-MM-DD, HH:mm')}</Text>
+                                <Text>{item.post_id}</Text>
+                                <Image
+                                    style={{ width: 150, height: 150 }}
+                                    source={{ uri: item.picture_uri }} // isideti normaliu nuotrauku, kad veiktu
+                                />
+                                <View style={{width: 100, height: 50, paddingTop: 20}}>
+                                    <Button  color="#f194ff" title='Delete' onPress={() =>{this.handleDeletion(item.post_id)}}/>
+                                </View>    
+                                <View style={{width: 100, height: 50}}>
+                                    <Button title='Update'onPress={() =>{this.updatePost(item.post_id)}}/>
+                                </View>                                   
+                            </View>}
+                            keyExtractor={item => item.post_id}
+                    />
+                </View>
             </ImageBackground>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    background: {
-        width: '100%',
-        height: '100%',
-    },
+  background: {
+      width: '100%',
+      height: '100%',
+  },
+  crud_header: {
+    fontSize: 25,
+    textAlign: 'center',
+    paddingBottom: 10
+    
+  }
 })
